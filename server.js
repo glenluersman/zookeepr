a3eea0c42a968328534882e24072b0d4d6b74ed7
glenluersman <glen.luersman@gmail.com>
2022-01-09 21:28:35 -0500

    front-end set up on server

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app =  express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 
 function filterByQuery(query, animalsArray) {
@@ -97,6 +98,18 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
